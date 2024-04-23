ec77ae45a81cf191044894ea06f3d375d07a6fb1
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-04-23 23:35:48 +0100

    changes

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import headerLogo from "../../assets/header-logo.svg";
 import { FaXTwitter, FaFacebook } from "react-icons/fa6"
 
@@ -15,30 +16,13 @@ const Footer = () => {
                         All rights reserved © PodA 2024
                     </p>
                     <div className="flex justify-center lg:justify-start gap-2 mt-3 lg:mt-0">
-                        <a
-                            href="https://x.com/Pod_Audio?s=09"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="bg-black p-4 rounded-full hover:bg-secondary transition duration-300 ease-in-out"
-                        >
-                            <div className="text-white"><FaXTwitter className="text-2xl" /></div>
-                        </a>
-                        <a
-                            href="https://www.facebook.com/profile.php?id=61555114434395&mibextid=ZbWKwL"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="bg-black p-4 rounded-full hover:bg-secondary transition duration-300 ease-in-out"
-                        >
-                            <div className="text-white"><FaFacebook className="text-2xl" /></div>
-                        </a>
-                        {/* <a
-                            href="https://instagram.com/example"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="bg-black p-4 rounded-full hover:bg-secondary transition duration-300 ease-in-out"
-                        >
-                            <div className="text-white"><FaInstagram className="text-2xl" /></div>
-                        </a> */}
+
+                        <Link to="https://www.facebook.com/profile.php?id=61555114434395&mibextid=ZbWKwL">
+                            <div className="mr-2"> <FaXTwitter className="text-2xl" /></div>
+                        </Link>
+                        <Link to="https://x.com/Pod_Audio?s=09">
+                            <div className="ml-2"> <FaFacebook className="text-2xl" /></div>
+                        </Link>
                     </div>
                 </div>
             </div>
